3af09274b8900f77cf8dccd6571f0b7783ab8da1
Empanada <ronald362001@gmail.com>
2022-08-28 22:39:44 -0600

    User type added

--- models/UsersModel.js
+++ models/UsersModel.js
@@ -1,27 +1,31 @@
 module.exports = (sequelize, DataTypes) => {
-    return sequelize.define('Users', {
-        UserId: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true,
-            allowNull: false,
-        },
-        UserName: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        UserPassword: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        UserEmail: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        UserVerified: {
-            type: DataTypes.BOOLEAN,
-            allowNull: false,
-            defaultValue: false,
-        },
-    });
-}
+  return sequelize.define("Users", {
+    UserId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    UserName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    UserPassword: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    UserEmail: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    UserVerified: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+    },
+    UserType: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+};
