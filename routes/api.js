274ba492dd94c87a13c8d6d7bd3afdc0e300723b
Empanada <ronald362001@gmail.com>
2022-09-04 10:02:20 -0600

    TravelRegistered routes complete

--- routes/api.js
+++ routes/api.js
@@ -2,9 +2,11 @@ const router = require("express").Router();
 const ApiTravelRoute = require("./api/TravelController");
 const ApiUsersRoute = require("./api/UserController");
 const ApiVehiclesRoute = require("./api/VehicleController");
+const ApiTravelRegisterRoute = require("./api/TravelRegisterController");
 
 router.use("/travels", ApiTravelRoute);
 router.use("/users", ApiUsersRoute);
 router.use("/vehicles", ApiVehiclesRoute);
+router.use("/travelRegister", ApiTravelRegisterRoute);
 
 module.exports = router;
---
+++ routes/api/TravelRegisterController.js
@@ -0,0 +1,69 @@
+const router = require("express").Router();
+const { Travel, TravelRegister } = require("../../db");
+
+router.get("/:UserId", async (req, res) => {
+  try {
+    const travels = await TravelRegister.findAll({
+      where: {
+        UserId: req.params.UserId,
+      },
+      include: [Travel],
+    });
+    res.status(200).json(travels);
+  } catch (err) {
+    res.status(401).json({ message: "Datos invalidos" });
+  }
+});
+
+router.get("/simplify/:UserId", async (req, res) => {
+  try {
+    const travels = await TravelRegister.findAll({
+      attributes: ["TravelRegisterId"],
+      where: {
+        UserId: req.params.UserId,
+      },
+      include: [
+        {
+          model: Travel,
+          attributes: [
+            "TravelId",
+            "TravelDate",
+            "TravelOrigin",
+            "TravelDestination",
+            "TravelPrice",
+          ],
+        },
+      ],
+    });
+    res.status(200).json(travels);
+  } catch (err) {
+    console.log(err);
+    res.status(401).json({ message: "Datos invalidos" });
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const travel = await TravelRegister.create(req.body);
+    res.status(200).json(travel);
+  } catch (err) {
+    res.status(401).json({ message: "Datos invalidos" });
+  }
+});
+
+router.delete("/:TravelRegisterId", async (req, res) => {
+    try {
+        const travel = await TravelRegister.destroy({
+            where: {
+                TravelRegisterId: req.params.TravelRegisterId
+            }
+        });
+        res.status(200).json(travel);
+    } catch (err) {
+        res.status(401).json({ message: "Datos invalidos" });
+    }
+});
+
+
+
+module.exports = router;
